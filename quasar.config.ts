67a06c9dbc403d02e257ef838cf092ac9071d3c8
darqus <voladores@inbox.ru>
2025-06-05 18:37:36 +0300

    update quasar.config.ts

--- quasar.config.ts
+++ quasar.config.ts
@@ -35,7 +35,7 @@ export default defineConfig((/* ctx */) => ({
       // extendTsConfig (tsConfig) {}
     },
 
-    vueRouterMode: 'hash', // available values: 'hash', 'history'
+    vueRouterMode: 'history', // available values: 'hash', 'history'
     // vueRouterBase,
     // vueDevtools,
     // vueOptionsAPI: false,
