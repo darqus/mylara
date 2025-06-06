3d2a1f525d43ec8d45f1f43b39cde6e3cdcc94b1
darqus <voladores@inbox.ru>
2025-06-06 13:42:41 +0300

    fix deploy

--- deploy-gh-pages.sh
+++ deploy-gh-pages.sh
@@ -3,8 +3,8 @@
 # Настройка переменных
 USERNAME="darqus"
 REPO="mylara"
-EMAIL="voladores@inbox.ru"  # Замените на свой email
-NAME="darqus"                # Замените на своё имя
+EMAIL="voladores@inbox.ru"
+NAME="darqus"
 
 # Сборка проекта
 echo "Сборка проекта..."
@@ -16,16 +16,6 @@ cd dist/spa
 # Создание файла .nojekyll для GitHub Pages
 touch .nojekyll
 
-# Копирование файла CNAME
-# cp ../CNAME .
-
-# Исправление путей в index.html
-# echo "Исправление путей в index.html..."
-# sed -i 's|href=/|href=|g' index.html
-# sed -i 's|src="/|src="|g' index.html
-# sed -i 's|href="/|href="|g' index.html
-# echo "Пути успешно исправлены!"
-
 # Инициализация Git репозитория
 git init -b main
 git config --local user.email "$EMAIL"
@@ -33,9 +23,15 @@ git config --local user.name "$NAME"
 git add .
 git commit -m "Deploy to GitHub Pages"
 
-# Пуш на ветку gh-pages
-# Используем HTTPS вместо SSH для более простой аутентификации
-git push -f https://github.com/$USERNAME/$REPO.git HEAD:gh-pages
+# Пуш на ветку gh-pages с использованием токена для аутентификации
+# Проверяем наличие переменной GITHUB_TOKEN
+if [ -z "$GITHUB_TOKEN" ]; then
+  echo "GITHUB_TOKEN не найден. Используем HTTPS URL."
+  git push -f https://github.com/$USERNAME/$REPO.git HEAD:gh-pages
+else
+  echo "Используем GITHUB_TOKEN для аутентификации."
+  git push -f https://$GITHUB_TOKEN@github.com/$USERNAME/$REPO.git HEAD:gh-pages
+fi
 
 # Возврат в корневую директорию
 cd -
