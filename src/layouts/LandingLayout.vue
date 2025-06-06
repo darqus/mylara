f60234179f8f53e0fecf9292381771ad37cd1729
darqus <voladores@inbox.ru>
2025-06-06 23:45:14 +0300

    add Footer

--- src/layouts/LandingLayout.vue
+++ src/layouts/LandingLayout.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import LandingFooter from './landing-footer/LandingFooter.vue'
 import LandingHeader from './landing-header/LandingHeader.vue'
 
 </script>
@@ -9,5 +10,6 @@ import LandingHeader from './landing-header/LandingHeader.vue'
     <q-page-container>
       <router-view />
     </q-page-container>
+    <LandingFooter />
   </q-layout>
 </template>
---
+++ src/layouts/landing-footer/LandingFooter.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <q-footer
+    class="bg-white text-primary"
+    elevated
+  >
+    <q-toolbar>
+      <q-img
+        height="auto"
+        src="logo.svg"
+        width="150px"
+      />
+    </q-toolbar>
+  </q-footer>
+</template>
